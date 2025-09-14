a35785493db81109c28aaf28d24bb81173927252
Sujit Kirpan <sujitkirpan@gmail.com>
2025-09-14 15:46:41 +0530

    Ready

--- db.js
+++ db.js
@@ -2,9 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: String(process.env.DB_PASS ),
+  user: process.env.DB_USER || "postgres",
+  host: process.env.DB_HOST || "localhost",
+  database: process.env.DB_NAME || "store_rating_db",
+  password: String(process.env.DB_PASS || "Sujit@123"),
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
 });
 
